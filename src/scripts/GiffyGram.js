1c19b70e913d0157de5017c491e1135b61c80480
Chelsea.harris <chelsea.harris@asurion.com>
2021-11-10 15:49:55 -0600

    message feature with non functioning buttons

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -6,8 +6,10 @@ export const GiffyGram = () => {
     // Show main main UI
     return `
         <h1>Giffygram</h1>
-        ${PostEntry()}
         ${navBar()}
+        ${PostEntry()}
+       
+        
         ${MessageUsers()}
     `
 }
--- src/scripts/message/MessageForm.js
+++ src/scripts/message/MessageForm.js
@@ -4,9 +4,9 @@ export const MessageUsers = () => { // eporting the desired html to the page
     const users = getUsers()
     let html= "" 
     // the fx below it the recipient drop down
-    html += ` <div> <label> Choose your user </label> 
+    html += ` <div> <label> Recipient: </label> </div>
     <select id="user"> 
-    <option value="0">Choose your user </option> 
+    <div><option value="0">Choose your friend </option> </div>
     `
          for (const user of users) { 
             html += `<option value="${user.id}" > ${user.name} </option>` 
@@ -15,10 +15,12 @@ export const MessageUsers = () => { // eporting the desired html to the page
     html+= "</select></div>"  
 // here we start the input field gor messages
 html +=  `<div class="field">
-<label class="label" for="serviceDescription">Description</label>
-<input type="text" name="serviceDescription" class="input" />
+<label class="label" for="Message box">What are you thinking?</label> </div>
+<div><textarea type="text" name="Message" class="textarea"> </textarea>
 </div>`
 
+html += `<button class = "send" type="button">send</button>
+<button class = "cancel" type="button">cancel</button>`
 return html
 }
 
