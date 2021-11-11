5c4adcced8a55970de91a282192efca4210c1ef8
Chelsea.harris <chelsea.harris@asurion.com>
2021-11-11 13:54:49 -0600

    added send feature to messages

--- src/scripts/message/MessageForm.js
+++ src/scripts/message/MessageForm.js
@@ -1,27 +1,29 @@
-import {getUsers} from "../data/provider.js"
+import { getUsers, setRecipient, saveMessage, getFeed } from "../data/provider.js"
+
+const users = getUsers()
 
 export const MessageUsers = () => { // eporting the desired html to the page
     const users = getUsers()
-    let html= "" 
+    let html = ""
     // the fx below it the recipient drop down
     html += ` <div> <label> Recipient: </label> </div>
-    <select id="user"> 
+    <select id="recipient"> 
     <div><option value="0">Choose your friend </option> </div>
     `
-         for (const user of users) { 
-            html += `<option value="${user.id}" > ${user.name} </option>` 
-        } 
-        
-    html+= "</select></div>"  
-// here we start the input field gor messages
-html +=  `<div class="field">
+    for (const user of users) {
+        html += `<option value="${user.id}" > ${user.name} </option>`
+    }
+
+    html += "</select></div>"
+    // here we start the input field gor messages
+    html += `<div class="field">
 <label class="label" for="Message box">What are you thinking?</label> </div>
 <div><textarea type="text" name="Message" class="textarea"> </textarea>
 </div>`
 
-html += `<button class = "send" type="button">send</button>
+    html += `<button id = "send" type="button">send</button>
 <button class = "cancel" type="button">cancel</button>`
-return html
+    return html
 }
 
 // save/ cancle buttons needed 
@@ -29,10 +31,29 @@ return html
 document.addEventListener(
     "click",
     (clickEvent) => {
-        if (changeEvent.target.id === "send") {
-            const sendClicked = changeEvent.target.value
-            window.alert(`${walker.name} services ${walker.city}`)
-        } 
-    
+        const feed = getFeed()
+        if (clickEvent.target.id === "send") {
+            const senderId = localStorage.getItem("gg_user")
+            const recipientId = feed.recipientid
+            const message = document.querySelector("textarea[name='Message']").value
+
+            const userMessage = {
+                senderId: parseInt(senderId),
+                recipientId: recipientId,
+                message: message,
+            }
+            saveMessage(userMessage)
+        }
     }
 )
+
+document.addEventListener(
+    "change",
+    (changeEvent) => {
+        if (changeEvent.target.id === "recipient") {
+            const recipientId = changeEvent.target.value
+            setRecipient(parseInt(recipientId))
+        }
+    }
+)
+
