af29aa8f5626790b0d25bfb722a1be3d2db7f239
luciidlou <lewisale96@gmail.com>
2021-11-11 13:13:06 -0600

    posts can now be deleted

--- src/scripts/feed/Post.js
+++ src/scripts/feed/Post.js
@@ -4,26 +4,43 @@ import { deletePost, getUsers } from "../data/provider.js"
 export const buildPost = (post) => {
     const users = getUsers()
     const foundUser = users.find(user => user.id === post.userId)
-    return `<div class="post">
-                <h2 class="post__title">${post.title}</h2>
-                <img class="post__image" src="${post.url}">
-                <div class="post__description">${post.description}</div>
-                <div class="post__tagline">Posted by ${foundUser.name}on 11/10/2021</div>
-                <div class="post__actions">
-                    <div>
-                        <img id="favoritePost--${post.id}" class="actionIcon" src="images/favorite-star-blank.svg">
-                    </div>
-                    <div>
-                        <img id="blockPost--${post.id}" class="actionIcon" src="images/block.svg">
-                    </div>
-                </div>
-            </div>`
+    for (const user of users) {
+        if (user.id === post.userId) {
+            return `<div class="post">
+                        <h2 class="post__title">${post.title}</h2>
+                        <img class="post__image" src="${post.url}">
+                        <div class="post__description">${post.description}</div>
+                        <div class="post__tagline">Posted by ${foundUser.name} on 11/10/2021</div>
+                        <div class="post__actions">
+                            <div>
+                                <img id="favoritePost--${post.id}" class="actionIcon" src="images/favorite-star-blank.svg">
+                            </div>
+                            <div>
+                                <img id="blockPost--${post.id}" class="actionIcon" src="images/block.svg">
+                            </div>
+                        </div>
+                    </div>`
+        }
+        else {
+            return `<div class="post">
+                        <h2 class="post__title">${post.title}</h2>
+                        <img class="post__image" src="${post.url}">
+                        <div class="post__description">${post.description}</div>
+                        <div class="post__tagline">Posted by ${foundUser.name} on 11/10/2021</div>
+                        <div class="post__actions">
+                            <div>
+                                <img id="favoritePost--${post.id}" class="actionIcon" src="images/favorite-star-blank.svg">
+                            </div>
+                        </div>
+                    </div>`
+        }
+    }
 }
 
 document.addEventListener("click",
-click => {
-    if (click.target.id.startsWith("blockPost--")) {
-        const [, postId] = click.target.id.split("--")
-        deletePost(parseInt(postId))
-    }
-})
+    click => {
+        if (click.target.id.startsWith("blockPost--")) {
+            const [, postId] = click.target.id.split("--")
+            deletePost(parseInt(postId))
+        }
+    })
